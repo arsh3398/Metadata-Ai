0111cd34c392da8904548441e0eee820c81a8a2e
arsh3398 <102918987+arsh3398@users.noreply.github.com>
2023-07-04 14:58:51 +0530

    New components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,9 +7,12 @@ import { HeaderComponent } from './components/header/header.component';
 import { HeroHeaderComponent } from './components/hero-header/hero-header.component';
 import { ClientsComponent } from './components/clients/clients.component';
 import { TimelineComponent } from './components/timeline/timeline.component';
+import { FeaturesComponent } from './components/features/features.component';
+import { ReviewsComponent } from './components/reviews/reviews.component';
+import { InformationSectionComponent } from './components/information-section/information-section.component';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, HeroHeaderComponent, ClientsComponent, TimelineComponent],
+  declarations: [AppComponent, HeaderComponent, HeroHeaderComponent, ClientsComponent, TimelineComponent, FeaturesComponent, ReviewsComponent, InformationSectionComponent],
   imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule],
   providers: [],
   bootstrap: [AppComponent],
