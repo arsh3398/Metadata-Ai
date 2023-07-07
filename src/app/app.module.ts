fd57595473e65b1b44103dc715efb3a8177551d4
arsh3398 <102918987+arsh3398@users.noreply.github.com>
2023-07-07 18:54:12 +0530

    6 new components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,9 +10,15 @@ import { TimelineComponent } from './components/timeline/timeline.component';
 import { FeaturesComponent } from './components/features/features.component';
 import { ReviewsComponent } from './components/reviews/reviews.component';
 import { InformationSectionComponent } from './components/information-section/information-section.component';
+import { SlineInDirective } from './Directives/sline-in.directive';
+import { SocialpostsComponent } from './components/socialposts/socialposts.component';
+import { WorldmapComponent } from './components/worldmap/worldmap.component';
+import { IntegrationComponent } from './components/integration/integration.component';
+import { DemoComponent } from './components/demo/demo.component';
+import { BannerComponent } from './components/banner/banner.component';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, HeroHeaderComponent, ClientsComponent, TimelineComponent, FeaturesComponent, ReviewsComponent, InformationSectionComponent],
+  declarations: [AppComponent, HeaderComponent, HeroHeaderComponent, ClientsComponent, TimelineComponent, FeaturesComponent, ReviewsComponent, InformationSectionComponent, SlineInDirective, SocialpostsComponent, WorldmapComponent, IntegrationComponent, DemoComponent, BannerComponent],
   imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule],
   providers: [],
   bootstrap: [AppComponent],
