42fc53ae5b7803b2b333f9647af94eb4df05061d
arsh3398 <102918987+arsh3398@users.noreply.github.com>
2023-07-11 18:17:04 +0530

    New component

--- src/app/components/customer-story/customer-story.component.ts
+++ src/app/components/customer-story/customer-story.component.ts
@@ -5,6 +5,19 @@ import { Component, HostBinding } from '@angular/core';
   templateUrl: './customer-story.component.html',
   styleUrls: ['./customer-story.component.scss']
 })
-export class CustomerStoryComponent {
 
+export class CustomerStoryComponent {
+  activeTab:number = 0;
+  makeActive(index:number){
+    this.activeTab=index;
+  }
+  stylesTab={
+    'filter':'grayscale(0%)',
+    'border-bottom': '3px solid black',
+  }
+  stylesPanel={
+    'display':'flex',
+    'align-items': 'center'
+  }
+  tablePanels:string[]=['assets/panel-1.jpg','assets/panel-2.jpg','assets/panel-3.jpg','assets/panel-4.jpg']
 }
